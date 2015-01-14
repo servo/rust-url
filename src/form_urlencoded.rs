1a3533e4e08c5cf69a66f8555fc200ebb62956b7
Corey Farwell <coreyf@rwell.org>
2015-01-14 00:14:47 -0500

    Bump copyright year on files I modified

--- src/form_urlencoded.rs
+++ src/form_urlencoded.rs
@@ -1,4 +1,4 @@
-// Copyright 2013-2014 Simon Sapin.
+// Copyright 2013-2015 Simon Sapin.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
