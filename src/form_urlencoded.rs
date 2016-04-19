adba760e296790e98beda1bda5fa5b183d983032
Simon Sapin <simon.sapin@exyr.org>
2016-04-19 18:24:41 +0200

    Copyright date
    Does it matter?

--- src/form_urlencoded.rs
+++ src/form_urlencoded.rs
@@ -1,4 +1,4 @@
-// Copyright 2013-2015 Simon Sapin.
+// Copyright 2013-2016 Simon Sapin.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
