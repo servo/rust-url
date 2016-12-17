7f319577d303c5ce6f7b25c7a6691d2ce89ec261
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-12-17 14:25:17 -0800

    Add an external serde code generator to cut down on compile time

---
+++ serde-code-generator/src/main.rs
@@ -0,0 +1,15 @@
+extern crate serde_codegen;
+
+use std::path::Path;
+
+pub fn main() {
+    // let src = Path::new("../src/codegen/url.rs.in");
+    // let dst = Path::new("../src/codegen/url.rs");
+    //
+    // serde_codegen::expand(&src, &dst).unwrap();
+
+    let src = Path::new("../src/codegen/host.rs.in");
+    let dst = Path::new("../src/codegen/host.rs");
+
+    serde_codegen::expand(&src, &dst).unwrap();
+}
