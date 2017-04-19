249369a528c1a93c41163e44c92d49fe0d8a95fa
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-04-19 19:45:06 +0100

    Updating fuzzing code to latest format

--- fuzz/fuzzers/parse.rs
+++ fuzz/fuzzers/parse.rs
@@ -1,16 +1,10 @@
 #![no_main]
-
-extern crate libfuzzer_sys;
-
+#[macro_use] extern crate libfuzzer_sys;
 extern crate url;
-use std::slice;
 use std::str;
 
-#[export_name="LLVMFuzzerTestOneInput"]
-pub extern fn go(data: *const u8, size: isize) -> i32 {
-	let slice = unsafe { slice::from_raw_parts(data, size as usize) };
-	if let Ok(utf8) = str::from_utf8(slice) {
-		let url = url::Url::parse(utf8);
-	}
-	return 0;
-}
+fuzz_target!(|data: &[u8]| {
+    if let Ok(utf8) = str::from_utf8(data) {
+        let _ = url::Url::parse(utf8);
+    }
+});
