4ceb629ea40a78f7dd943ab45c9660ca85b7020a
Stanislav Tkach <stanislav.tkach@xdev.re>
2017-05-05 23:19:32 +0300

    Add html_root_url attribute

--- src/lib.rs
+++ src/lib.rs
@@ -90,6 +90,8 @@ let css_url = this_document.join("../main.css").unwrap();
 assert_eq!(css_url.as_str(), "http://servo.github.io/rust-url/main.css")
 */
 
+#![doc(html_root_url = "https://docs.rs/url/1.4.0")]
+
 #[cfg(feature="rustc-serialize")] extern crate rustc_serialize;
 #[macro_use] extern crate matches;
 #[cfg(feature="serde")] extern crate serde;
