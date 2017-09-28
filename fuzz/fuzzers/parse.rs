4b5a4222bb58f2345c251dd068e16851da5c4e24
Corey Farwell <coreyf@rwell.org>
2017-09-27 22:20:31 -0400

    Extend fuzz target to also do serialization.

--- fuzz/fuzzers/parse.rs
+++ fuzz/fuzzers/parse.rs
@@ -5,6 +5,9 @@ use std::str;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(utf8) = str::from_utf8(data) {
-        let _ = url::Url::parse(utf8);
+        if let Ok(parsed) = url::Url::parse(utf8) {
+            let as_str = parsed.as_str();
+            assert_eq!(parsed, url::Url::parse(as_str).unwrap());
+        }
     }
 });
