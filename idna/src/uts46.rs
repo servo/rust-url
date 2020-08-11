049f8cc7c07bf9ff9d98daa8ba33b4b6bf9000c0
ltdk <usr@ltdk.xyz>
2020-08-11 18:51:13 -0400

    Dissallowed => Disallowed, use enumerate instead of split_first

--- idna/src/uts46.rs
+++ idna/src/uts46.rs
@@ -95,18 +95,18 @@ fn map_char(codepoint: char, config: Config, output: &mut String, errors: &mut V
             }
         }
         Mapping::Disallowed => {
-            errors.push(Error::DissallowedCharacter);
+            errors.push(Error::DisallowedCharacter);
             output.push(codepoint);
         }
         Mapping::DisallowedStd3Valid => {
             if config.use_std3_ascii_rules {
-                errors.push(Error::DissallowedByStd3AsciiRules);
+                errors.push(Error::DisallowedByStd3AsciiRules);
             }
             output.push(codepoint)
         }
         Mapping::DisallowedStd3Mapped(ref slice) => {
             if config.use_std3_ascii_rules {
-                errors.push(Error::DissallowedMappedInStd3);
+                errors.push(Error::DisallowedMappedInStd3);
             }
             output.push_str(decode_slice(slice))
         }
@@ -483,22 +483,22 @@ enum Error {
 
     // https://unicode.org/reports/tr46/#Validity_Criteria
     ValidityCriteria,
-    DissallowedByStd3AsciiRules,
-    DissallowedMappedInStd3,
-    DissallowedCharacter,
+    DisallowedByStd3AsciiRules,
+    DisallowedMappedInStd3,
+    DisallowedCharacter,
     TooLongForDns,
     TooShortForDns,
 }
 impl Error {
     fn as_str(&self) -> &str {
         match self {
-            PunycodeError => "punycode error",
-            ValidityCriteria => "failed UTS #46 validity criteria",
-            DissallowedByStd3AsciiRules => "disallowed ASCII character",
-            DissallowedMappedInStd3 => "disallowed mapped ASCII character",
-            DissallowedCharacter => "disallowed non-ASCII character",
-            TooLongForDns => "too long for DNS",
-            TooShortForDns => "too short for DNS",
+            Error::PunycodeError => "punycode error",
+            Error::ValidityCriteria => "failed UTS #46 validity criteria",
+            Error::DisallowedByStd3AsciiRules => "disallowed ASCII character",
+            Error::DisallowedMappedInStd3 => "disallowed mapped ASCII character",
+            Error::DisallowedCharacter => "disallowed non-ASCII character",
+            Error::TooLongForDns => "too long for DNS",
+            Error::TooShortForDns => "too short for DNS",
         }
     }
 }
@@ -519,11 +519,11 @@ impl StdError for Errors {}
 
 impl fmt::Display for Errors {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let (first, rest) = self.0.split_first().expect("should be at least one error");
-        f.write_str(first.as_str())?;
-        for x in rest {
-            f.write_str(", ")?;
-            f.write_str(x.as_str())?;
+        for (i, err) in self.0.iter().enumerate() {
+            if i > 0 {
+                f.write_str(", ")?;
+            }
+            f.write_str(err.as_str());
         }
         Ok(())
     }
