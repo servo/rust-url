99a74880595dc5a4ca058cfa65e76c088d9afc21
Chance <chanceusc@gmail.com>
2023-08-04 11:21:26 -0400

    applies the same value check to set_port

--- url/src/lib.rs
+++ url/src/lib.rs
@@ -1770,6 +1770,9 @@ impl Url {
     pub fn set_port(&mut self, mut port: Option<u16>) -> Result<(), ()> {
         // has_host implies !cannot_be_a_base
         if !self.has_host() || self.host() == Some(Host::Domain("")) || self.scheme() == "file" {
+            if port.is_some() {
+                return Ok(());
+            }
             return Err(());
         }
         if port.is_some() && port == parser::default_port(self.scheme()) {
