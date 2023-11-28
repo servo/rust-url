4fbc5a60dc96829a721d6c23f84c1222ca826be5
Michael Farrell <micolous+git@gmail.com>
2023-11-28 19:05:06 +1000

    stray merge

--- url/src/lib.rs
+++ url/src/lib.rs
@@ -1226,7 +1226,6 @@ impl Url {
         self.port.or_else(|| parser::default_port(self.scheme()))
     }
 
-    #[cfg(not(target_arch = "wasm32"))]
     /// Resolve a URL’s host and port number to `SocketAddr`.
     ///
     /// If the URL has the default port number of a scheme that is unknown to this library,
@@ -1235,8 +1234,6 @@ impl Url {
     /// or by matching on the URL’s `.scheme()`.
     ///
     /// If the host is a domain, it is resolved using the standard library’s DNS support.
-    ///
-    /// **Note:** this feature is not available on `target_arch = "wasm32"`.
     ///
     /// # Examples
     ///
