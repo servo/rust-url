e423c6c5e94c4bf4d5483f4a4e7932ff333410d4
Michael Farrell <micolous+git@gmail.com>
2023-11-28 18:43:28 +1000

    fix lints

--- url/tests/unit.rs
+++ url/tests/unit.rs
@@ -597,6 +597,7 @@ fn test_origin_opaque() {
 
 #[test]
 #[cfg(not(target_arch = "wasm32"))]
+#[allow(deprecated)]
 fn test_origin_unicode_serialization() {
     let data = [
         ("http://😅.com", "http://😅.com"),
@@ -606,7 +607,6 @@ fn test_origin_unicode_serialization() {
     ];
     for &(unicode_url, expected_serialization) in &data {
         let origin = Url::parse(unicode_url).unwrap().origin();
-        #[allow(deprecated)]
         assert_eq!(origin.unicode_serialization(), *expected_serialization);
     }
 
@@ -626,7 +626,6 @@ fn test_origin_unicode_serialization() {
         Url::parse("http://127.0.0.1").unwrap().origin(),
     ];
     for ascii_origin in &ascii_origins {
-        #[allow(deprecated)]
         assert_eq!(
             ascii_origin.ascii_serialization(),
             ascii_origin.unicode_serialization()
