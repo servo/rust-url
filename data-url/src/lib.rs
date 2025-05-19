c54d5187dfc3fbe60a573d07453ff5e7f12d79bb
Martin Robinson <mrobinson@igalia.com>
2025-05-19 11:15:51 +0200

    Add some some basic functions to `Mime`
    This adds `Clone`, `fn new()`, and `fn matches()` to `Mime`. Servo uses
    `data_url::Mime` in places as it supports charsets and these helpers
    make it a lot nicer to use.
    
    Signed-off-by: Martin Robinson <mrobinson@igalia.com>

--- data-url/src/lib.rs
+++ data-url/src/lib.rs
@@ -8,8 +8,7 @@
 //! let url = DataUrl::process("data:,Hello%20World!").unwrap();
 //! let (body, fragment) = url.decode_to_vec().unwrap();
 //!
-//! assert_eq!(url.mime_type().type_, "text");
-//! assert_eq!(url.mime_type().subtype, "plain");
+//! assert!(url.mime_type().matches("text", "plain"));
 //! assert_eq!(url.mime_type().get_parameter("charset"), Some("US-ASCII"));
 //! assert_eq!(body, b"Hello World!");
 //! assert!(fragment.is_none());
