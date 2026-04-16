9a04be18b36cd08e933da06554af0d83bf3fb31b
fox0 <15684995+fox0@users.noreply.github.com>
2026-04-16 21:13:37 +0700

    doc: An extra empty line has been deleted. (#1115)
    Signed-off-by: fox0 <15684995+fox0@users.noreply.github.com>

--- url/src/lib.rs
+++ url/src/lib.rs
@@ -32,7 +32,6 @@ let issue_list_url = Url::parse(
     "https://github.com/rust-lang/rust/issues?labels=E-easy&state=open"
 )?;
 
-
 assert!(issue_list_url.scheme() == "https");
 assert!(issue_list_url.username() == "");
 assert!(issue_list_url.password() == None);
